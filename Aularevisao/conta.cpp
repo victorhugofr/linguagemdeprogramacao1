a28eb25cc164f4883154637a753906bf31e0fe93
Victor Hugo <vale1c@ufrn.edu.br>
2018-04-04 23:55:59 -0300

    removendo

--- Aularevisao/conta.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "conta.hpp"
-int Conta::getagencia(){return agencia;}
-int Conta::getnumero(){return numero;}
-int Conta::getsaldo(){return saldo;}
-void Conta::setagencia(int a){agencia=a;}
-void Conta::setnumero(int a){numero=a;}
-void Conta::setsaldo(int a){saldo=a;}
