9793ff89d9ce5f4758fc018bd288f004b39d275c
Victor Hugo <vale1c@ufrn.edu.br>
2018-06-01 20:17:10 -0300

    remove files

--- Aula07/imprimir.h
+++
@@ -1,8 +0,0 @@
-#ifndef IMPRIMIR_H
-#define IMPRIMIR_H
-/**
-*@brief funcao que imprime o texto na tela
-*@details contém o resultado da corrida na impressao
-*/
-void imprimir(int ganhador, Sapo *sapo);
-#endif
