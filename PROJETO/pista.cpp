95978b3c36e8efe2f6f2b042df62aeed2c9aedd5
Victor Hugo <vale1c@ufrn.edu.br>
2018-06-06 21:29:51 -0300

    removendo

--- PROJETO/pista.cpp
+++
@@ -1,7 +0,0 @@
-#include "pista.hpp"
-using namespace std;
-ostream& operator<< (ostream &o,Pista &pistas){
-
-    o <<"Nome da pista: "<<pistas.nome<<endl<<"Distancia: "<<pistas.distancia<<endl<<"*************"<<endl;
-	return o;
-}
